966d40448aac151ee3b6637bf43c1100493b4c4e
joshkgw <jkgwhite2005@gmail.com>
2024-12-16 12:27:35 +0000

    essential classes declared

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+import dataclass
 import pyside6
+
+
+
+
+@dataclass
+class Stock:
+    pass
+
+
+@dataclass
+class Bicycle:
+    pass
+
+
+@dataclass
+class WeldingStation:
+    pass
+
+
+@dataclass
+class AssemblyStation:
+    pass
+
 
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()
